26986b20d30008b62f4022a9943bfba73ef59de1
admin <kuligturan@mail.ri>
2022-08-16 14:25:05 +0600

    fix

--- cinematica/settings.py
+++ cinematica/settings.py
@@ -130,12 +130,14 @@ USE_TZ = True
 
 
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.SessionAuthentication',
-        'rest_framework.authentication.BasicAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-    ),
+    ]
+
 }
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
